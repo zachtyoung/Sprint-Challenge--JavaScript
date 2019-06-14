54d965ff3d02751d6f58909a312beefa5d76cc59
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-06-14 11:06:58 -0500

    WIP: syntax fix

--- challenges/functions.js
+++ challenges/functions.js
@@ -27,7 +27,7 @@ let greeting = function(first, last){
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
- console.logconsume(2,2,add)); // 4
+ console.log(consume(2,2,add)); // 4
   console.log(consume(10,16,multiply)); // 160
 console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
